fda456b98c7647542157f3f86abd5ea0c375fe60
mariana-leite92 <leite.mari92@gmail.com>
2021-01-01 23:42:49 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoPessoaDDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
